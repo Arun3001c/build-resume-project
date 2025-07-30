97a268044fc673d30d08de61897ff95dce59ec9c
Arun <pmsak28@gmail.com>
2025-07-30 22:55:41 +0530

    Replaced old resume builder code with new Vite-based version

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AcademicPersonalForm from "./resume.jsx";
+import ResumeDash from "./resumeDash.jsx";
+
+function App() {
+  
+  return (
+    <>
+     <AcademicPersonalForm/>
+    {/* < ResumeDash/> */}
+    </>
+  )
+}
+
+export default App
